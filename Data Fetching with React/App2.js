1dd137b3c3f9dcc438ecc7759ad995c3cf0ee38b
ChadCorpin <116444454+ChadCorpin@users.noreply.github.com>
2022-11-17 21:58:43 +0800

    Create App2.js

---
+++ Data Fetching with React/App2.js
@@ -0,0 +1,9 @@
+const greeting = 'Hello';
+// + operator
+const welcome = greeting + ' React';
+console.log(welcome);
+// Hello React
+// template literals
+const anotherWelcome = `${greeting} React`;
+console.log(anotherWelcome);
+// Hello React
